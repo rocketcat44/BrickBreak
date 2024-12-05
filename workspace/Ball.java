9e25d227f26fa0981f37da9dbcf973616c8012d5
rocketcat44 <blixinran@gmail.com>
2024-12-05 13:48:03 +0000

    commit -m "sumbission 12/5"

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -4,10 +4,61 @@ public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+	private int xPos;
+	private int yPos;
+	private int size;
+	private int xVelocity;
+	private int yVelocity;
 	
 	//constructor(s):
+
+	public Ball(int x, int y, int s){
+		xPos = x;
+		yPos = y;
+		size = s;
+	}
 	
 	//methods:
+
+
+	//getters and setters
+	int getYpos(){return this.yPos;}
+
+	int getXpos(){return this.xPos;}
+
+	int getSize(){return this.size;}
+
+	int getXVel(){return this.xVelocity;}
+
+	int getYVel(){return this.yVelocity;}
+
+	void setXVelocity(int x){this.xVelocity = x;}
+
+	void setYVelocity(int x){this.yVelocity = x;}
+
+	void setX(int x){this.xPos = x;}
+	void setY(int y){this.yPos = y;}
+
+
+
+
+	void draw(Graphics g){
+		g.setColor(Color.blue); // call first to set the desired color
+		g.fillOval(getXpos(), getYpos(), getSize(), getSize()); // draws an oval (ball)
+	}
+
+	void reverseY(){
+		setYVelocity(getYVel()*(-1));
+	}
+
+	void reverseX(){
+		setXVelocity(getXVel()*(-1));
+	}
+
+	void move(){
+		setX(getXpos()+getXVel());
+		setY(getYpos()+getYVel());
+	}
+
 
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,51 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
 	//your code here!
-	
-	//don't forget you need instance variables:
-	
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+	private int xVelocity;
 	
 	//constructor(s):
+	public Paddle(int x, int y, int w, int h){
+		this.xPos = x;
+		this.yPos = y;
+		this.width = w;
+		this.height = h;
+	}
+	//methods: getters and setters
+	int getX(){return this.xPos;}
+
+	int getY(){return this.yPos;}
+
+	int getWidth(){return this.width;}
+
+	int getHeight(){return this.height;}
+
+	int getVelocity(){return this.xVelocity;}
+
+	void setX(int x){this.xPos= x;}
+	void setVelocity(int x){this.xVelocity = x;}
+
+
+	// functions
+	void move(){
+		setX(getX()+getVelocity());
+	}
+
+	void addVelocity(int x){
+		setVelocity(x);
+	}
+
 	
-	//methods:
+	void draw(Graphics g){
+		g.setColor(Color.white); // call first to set the desired color
+        g.fillRect(getX() , getY(), getWidth(), getHeight()); // draws a rectangle (paddle)
+	}
 	
+
 	
 }
